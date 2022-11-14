64337ac4aacb312ab5035c8b4fbe1b2da0e8c23f
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-11-14 12:08:17 +0200

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import Axios from "axios";
 import SimpleLightbox from "simplelightbox";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+function getImages(search, page = 1) {
+	Axios.get('/user', {
+		params: {
+			key: "31198912-785fc91d0a48dd5c6d0fb52b2",
+			q: search,
+			image_type: photo,
+			orientation: horizontal,
+			safesearch: true
+		}
+	})
+		.then(response => response.data)
+		.then(response => {
+
+		})
+		.catch(error => console.log(error))
+}
